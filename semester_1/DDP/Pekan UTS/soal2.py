0618dba6562b2ae8ae6608544fdae0ede319dc30
Zulkifli Raihan <zuran2907@gmail.com>
2021-11-20 09:02:45 +0700

    Soal UTS DDP Nomor 2

---
+++ semester_1/DDP/Pekan UTS/soal2.py
@@ -0,0 +1,8 @@
+daftar_pegawai = [
+    {"nama": "Budi", "gaji": 1500000},
+    {"nama": "Cici", "gaji": 2000000}
+]
+
+print("|","Nama","|","Gaji","  ","|")
+print("|",daftar_pegawai[0]["nama"],"|",daftar_pegawai[0]["gaji"],"|")
+print("|",daftar_pegawai[1]["nama"],"|",daftar_pegawai[1]["gaji"],"|")
